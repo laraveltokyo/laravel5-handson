31e0320336e65ba2e21f6570555d08ca3a07f5db
Fumio Furukawa <jumilla@laravel.tokyo>
2015-10-15 09:34:21 +0900

    Small tweaks.

--- app/Providers/DatabaseServiceProvider.php
+++ app/Providers/DatabaseServiceProvider.php
@@ -18,13 +18,5 @@ class DatabaseServiceProvider extends ServiceProvider
         $this->migrations('framework', [
             '1.0' => Migrations\Framework_1_0::class,
         ]);
-
-        // $this->migrations('app', [
-        //     '1.0' => Migrations\App_1_0::class,
-        // ]);
-
-        // $this->seeds([
-        //     'test' => Seeds\Test::class,
-        // ], 'test');
     }
 }
